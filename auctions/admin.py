022cbde7eaff826ac328faa74ffc8f848787ff95
Donato Casiello <donato@MacBook-di-Donato.local>
2023-03-02 13:00:58 +0100

    Customise Auction and User models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Auction, User
+
 # Register your models here.
+class AuctionAdmin(admin.ModelAdmin):
+    list_display = "id", "title", "bid"
+
+admin.site.register(Auction, AuctionAdmin)
+admin.site.register(User)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Auction(models.Model):
+    title = models.CharField(max_length=50)
+    bid = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    
+    def __str__(self):
+        return f"{self.title} with bid: {self.bid} and id {self.id}"
+    
