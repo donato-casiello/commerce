a4570f76bee66aef8ebc8993879d3ff22bdb7b55
Donato Casiello <donato@MacBook-di-Donato.local>
2023-03-02 18:22:20 +0100

    Finally resolves the issues with database

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Auction
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    list_display = "id", "username"
+    list_display = "id", "username" 
     
 admin.site.register(User, UserAdmin)
+admin.site.register(Auction)
--- auctions/models.py
+++ auctions/models.py
@@ -5,3 +5,10 @@ from django.contrib.auth.models import AbstractUser
 # An Abstract User is a class of user that can be easily change if the need
 class User(AbstractUser):
     pass
+
+class Auction(models.Model):
+    title = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title, self.owner
