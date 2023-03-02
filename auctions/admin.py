837793ca331903f079347ecbb599af4ba28c8d22
Donato Casiello <donato@MacBook-di-Donato.local>
2023-03-02 18:54:30 +0100

    Create Bid table

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Auction
+from .models import User, Auction, Bid
 
 # Register your models here.
 """class UserAdmin(admin.ModelAdmin):
@@ -8,3 +8,4 @@ from .models import User, Auction
     
 admin.site.register(User)
 admin.site.register(Auction)
+admin.site.register(Bid)
--- auctions/models.py
+++ auctions/models.py
@@ -7,8 +7,15 @@ class User(AbstractUser):
     pass
 
 class Auction(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=200)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"'{self.title}' posted by {self.owner}"
+        return f"'{self.title}' posted by {self.owner} is active: {self.active}"
+    
+class Bid(models.Model):
+    auction_id = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
