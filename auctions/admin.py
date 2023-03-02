d0762b8f540131f2d23673b1dc1e89a1642af2e4
Donato Casiello <donato@MacBook-di-Donato.local>
2023-03-02 16:56:54 +0100

    Restore user setting

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Auction, User
+
+# Register your models here.
+
+# This class is used to display a particular way in admin interface 
+class AuctionAdmin(admin.ModelAdmin):
+    list_display = "id", "title", "bid"
+
+admin.site.register(Auction, AuctionAdmin)
+admin.site.register(User)
+
--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,14 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
+# An Abstract User is a class of user that can be easily change if the need
 class User(AbstractUser):
     pass
+
+class Auction(models.Model):
+    title = models.CharField(max_length=50)
+    bid = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    
+    def __str__(self):
+        return f"{self.title} with bid: {self.bid} and id {self.id}"
